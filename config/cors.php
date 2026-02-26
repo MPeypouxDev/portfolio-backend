07a1141ca547563a3113d56ca646acd954ea352f
Mathys <M.PeypouxDev@gmail.com>
2026-02-26 15:45:42 +0100

    fix: remove whitespace in cors.php

--- config/cors.php
+++ config/cors.php
@@ -4,12 +4,12 @@ return [
     'paths' => ['api/*', 'storage/*', 'icons/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-    
+
     'allowed_origins' => [
         'http://localhost:5173',
         'https://portfolio-frontend-tan-five.vercel.app',
     ],
-    
+
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
